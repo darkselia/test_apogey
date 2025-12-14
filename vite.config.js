ba18ec5e83253da5b6ed12659b917d3f625bb0c5
darkselia <darkselia@users.noreply.github.com>
2025-12-14 18:01:29 +0700

    build: configure base path for GitHub Pages deployment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1]
 // https://vite.dev/config/
 export default defineConfig({
+  base: repo ? `/${repo}/` : "/",
   plugins: [
     vue(),
     vueDevTools(),
